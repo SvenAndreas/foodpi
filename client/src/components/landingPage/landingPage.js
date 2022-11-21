8512ed88860709dbcc38b12ae5bb83cf964d52b4
unknown <svenandreasclausz@gmail.com>
2022-11-21 02:25:11 -0300

    changes in landing page, component 404 not found

--- client/src/components/landingPage/landingPage.js
+++ client/src/components/landingPage/landingPage.js
@@ -8,20 +8,26 @@ function LandingPage() {
  
    
   return (
-    <div className={[s.container,s.content].join(" ")}>
+    <div className={s.container}>
         <div className={s.videoContainer}>
             <video src={intro} autoPlay loop preload='true' muted ></video>
+            
         </div>
 
-        <div className={s.overlayContainer}> </div>
-
+        <div className={s.overlayContainer}> 
+          
+        </div>
+        
         <div className={s.contentContainer}>
             <h1>Food App</h1>
-            <p>Welcome to my personal project for Henry's bootcamp</p>
             <Link to="/home">
               <button className={s.btn}> Enter App</button>
             </Link>
             <audio src={music}  autoPlay preload='none' loop></audio>
+        </div>
+
+        <div style={{position:"absolute",zIndex:"100"}}>
+          <p className={s.contentContainer_p}>Welcome to my personal project for Henry's bootcamp</p>
         </div>
         
     </div>
--- client/src/components/notFound/NotFound.js
+++ client/src/components/notFound/NotFound.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import notFound from "../../media/images/notFound.jpg"
+import s from "./NotFound.module.css"
+import {Link} from "react-router-dom"
 
 function NotFound() {
   return (
-    <h1>NotFound</h1>
+    <div className={s.container}>
+      <div className={s.container_titleBtn}>
+        <h1>Error 404 not found</h1>  
+        <Link to="/home">
+          <button>Home</button>
+        </Link>
+      </div>
+      <img src={notFound} alt="notfound"></img>
+    </div>
   )
 }
 
